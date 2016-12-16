68df996576dd55d61bf924ba0349ae9cb6689730
Pajinsi <twiztedbull@gmail.com>
2016-12-15 21:44:54 -0600

    updated action list

--- sample/parser.py
+++ sample/parser.py
@@ -1,6 +1,7 @@
 #!/root/Python-3.5.2/python 
 import os
 import sys
+from _ssl import err_codes_to_names
 
 class Parser():
     def __init__(self,data):
@@ -8,11 +9,15 @@ class Parser():
     def x_header_parse(self):
         """Returns a dictionary of X Headers"""
         ret={}#parse the X Headers on 
-        for value in self.data.split(' '):
-            print(value)
-            if "=" in value:
-               ret[value.split('=')[0]] = value.split('=')[1]
-            continue
+        try:
+            for value in self.data.split(' '):
+                #print(value)
+                if "=" in value:
+                   ret[value.split('=')[0]] = value.split('=')[1]
+                ret['x_header_parse_bool']=True
+        except ValueError as err:
+            ret['x_header_parse_bool']=False
+            ret['x_header_err']=err
         return(ret)
     
-#print(Parser('ADDR=2.2.2.2  PROTO=ESMTP HELO=[1.2.3.4] IDENT=39B4741C4547 SOURCE=REMOTE').x_header_parse())
+#print(Parser('ADDR=2.2.2.2         PROTO=ESMTP HELO=[1.2.3.4] IDENT=39B4741C4547 SOURCE=REMOTE').x_header_parse())
